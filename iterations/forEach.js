ac4b4863d915eabe506796eb1a65d51335835157
Mian Ateeq-star <mr.unknown5264@gmail.com>
2024-01-04 18:38:57 +0500

    forEach

---
+++ iterations/forEach.js
@@ -0,0 +1,30 @@
+const coding = ["js","ruby","python","java","cpp"]
+
+
+coding.forEach((str,index,integer)=>{
+    console.log(str,index,integer);
+})
+
+
+
+
+const mycoding = [
+{
+    languageName:"javascript",
+    languageFileName:"js"
+},
+{
+    languageName:"java",
+    languageFileName:"java"
+},
+{
+    languageName:"ruby",
+    languageFileName:"rb"
+},
+]
+
+
+mycoding.forEach((lang)=>{
+console.log(lang.languageName);
+console.log(lang.languageFileName);
+})
