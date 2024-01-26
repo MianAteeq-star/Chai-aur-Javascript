a525b882f12aaded1e7e10ed8a30f17a8d2bc0cb
Mian_Ateeq <mr.unknown5264@gmail.com>
2024-01-26 20:02:48 +0500

    complete game

--- Projects/Guess the Number Game/script.js
+++ Projects/Guess the Number Game/script.js
@@ -1,5 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1)
-//  console.log(randomNumber);
+let randomNumber = parseInt(Math.random() * 100 + 1)
 
 const userInputValue = document.querySelector("#userValue")
 const submitButton = document.querySelector("#submitBtn")
@@ -7,7 +6,6 @@ const remainingGuesses = document.querySelector(".remaining")
 const previousValues = document.querySelector("#previous")
 const gameOverResult = document.querySelector("#prev")
 const resultMessage = document.querySelector("#result")
-const form = document.querySelector("form")
 
 
 let remainingValues = 1
@@ -21,35 +19,39 @@ if (playGame) {
         e.preventDefault()
         const userValue = parseInt(userInputValue.value)
 
+validate(userValue)
+    
+    })
+}
+function validate(userValue){
+    if (isNaN(userValue)) {
+        alertMessage("Please enter a valid number")
+    } else if (userValue < 1) {
+        alertMessage("Please enter a valid")
+    }
+    else if (userValue > 100) {
+        alertMessage("Please enter number less than 100")
+    } else {
 
-        if (isNaN(userValue)) {
-            alertMessage("Please enter a valid number")
-        } else if (userValue < 1) {
-            alertMessage("Please enter a valid")
-        }
-        else if (userValue > 100) {
-            alertMessage("Please enter number less than 100")
+        previousGuess.push(userValue)
+        if (remainingValues == 3) {
+            displayGuess(userValue)
+            alertMessage(`Game over. Random number was ${randomNumber} `)
+            endGame()
+            
         } else {
 
-            previousGuess.push(userValue)
-            if (remainingValues == 11) {
-                displayGuess(userValue)
-                alertMessage(`Game over. Random number was ${randomNumber} `)
-                playGame = false
-            } else {
-
-                checkGuess(userValue)
-                displayGuess(userValue)
-            }
+            checkGuess(userValue)
+            displayGuess(userValue)
         }
-    })
+    }
 }
 
 function checkGuess(guess) {
     if (guess === randomNumber) {
         alertMessage(`You got it! The number was ${randomNumber}`)
         endGame()
-        playGame = false
+    
     } else if (guess > randomNumber) {
         alertMessage(`Your guess is too high. `)
     }
@@ -65,24 +67,37 @@ function alertMessage(message) {
 
 function displayGuess(guess) {
     userInputValue.value = "";
-    remainingGuesses.innerHTML = `${11 - remainingValues}`
     previousValues.innerHTML += ` ${guess} `
     remainingValues++
+    remainingGuesses.innerHTML = `${11 - remainingValues}`
 }
 
-function endGame (game ){
-    userInputValue = "";
-    userInputValue.setAttribute("disabled", "")
-p.classList.add("button")
-p.innerHTML = ` <h1 id="newGame">Start new Game</h1>`
-gameOverResult.appendChild( p )
-playGame= false
-newGame()
+function endGame (){
+    userInputValue.value = "";
+    userInputValue.setAttribute('disabled', "")
+    p.classList.add("button")
+    p.style.fontSize= "20px"
+    p.style.color = "white"
+    p.innerHTML = '<h1 id="newGame">Start new Game</h1>';
+     gameOverResult.appendChild(p);
+    playGame= false
+    newGame()
 }
-function newGame (game ){
-    userInputValue = "";
-    playGame= true
-    previousGuess = []
-    remainingValues
+function newGame (){
+    const newGameResult = document.querySelector('#newGame');
+    console.log(newGameResult);
+    
+    newGameResult.addEventListener("click", function (e) {
+        e.preventDefault()
+        randomNumber = parseInt(Math.random() * 100 + 1)
+        userInputValue.value = "";
+        previousGuess = []
+        remainingValues = 1
+        remainingGuesses.innerHTML = ` ${11 - remainingValues}`
+        userInputValue.removeAttribute('disabled')
+        gameOverResult.removeChild(p)
+        playGame= true
+    
+    })
 }
 
